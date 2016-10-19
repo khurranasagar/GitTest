e76c679caef08e7f01d3941a206b3cb65258b359
khurranasagar <sagar15167@iiitd.ac.in>
2016-10-19 10:20:13 +0530

    Adding Tic_Tac_Play.java

---
+++ Tic_Tac_Play.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Tic_Tac_Play {
+
+	public static JFrame Game_Window = new JFrame();
+	
+	public static void main(String[] args){
+		
+		Game_Window.setTitle("Tic Tac Toe");
+		Game_Window.setSize(500,500);
+		Game_Window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		Game_Window.setVisible(true);
+		
+	}
+	
+	
+	
+	
+	
+}
